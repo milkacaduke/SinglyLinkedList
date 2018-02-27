b82850a6ff6ed2980d2d96b78911d708871ea5d4
Mike Jang <milkacaduke@gmail.com>
2018-02-26 20:14:24 -0800

    delete at index

--- SinglyLinkedList.py
+++ SinglyLinkedList.py
@@ -25,8 +25,9 @@ class LinkedList(object):
 
 	def insert_tail(self, data):
 		if self.head is None:
-			print("Linked List empty")
-			return False
+			newNode = LLNode(data)
+			self.head = newNode
+			return
 
 		curNode = self.head
 		while curNode.next:
@@ -92,6 +93,49 @@ class LinkedList(object):
 		prevNode.next = None
 		return curNode
 
+	def delete_at_index(self, idx):
+		print("delete_at_index {}".format(idx))
+		if idx is None:
+			print("index not provided")
+			return
+
+		if idx < 0:
+			print("index provided invalid")
+			return
+
+		if self.head is None:
+			print("Linked List empty")
+			return
+
+		if idx == 0 and self.head.next is None: # special case remove at head and LL only one node
+			self.head = None
+			return
+
+		i = 0
+		curNode = self.head
+		prevNode = None
+
+		while curNode:
+			if i == idx:
+				if prevNode is None: # remove at head
+					# we could just call self.delete_head()
+					temp = self.head
+					self.head = self.head.next
+					return temp
+				else:
+					prevNode.next = curNode.next
+					return curNode
+			i += 1
+			prevNode = curNode
+			curNode = curNode.next
+
+		print("index provided is out of bounds")
+		return
+
+
+
+
+
 
 	def length(self):
 		if self.head is None:
@@ -130,18 +174,21 @@ ll = LinkedList()
 
 ll.insert('A')
 ll.insert('B')
-ll.insert('C')
-ll.insert('D')
-ll.insert('E')
-# ll.insert_tail('F')
+# ll.insert('C')
+# ll.insert('D')
+# ll.insert('E')
 # # print(ll.head.data)
 
 ll.printAll()
 
-ll.insert_after_data('E', 'E1')
-ll.insert_after_data('C', 'C1')
-ll.insert_after_data('B', 'B1')
 ll.printAll()
+
+
+# ll.insert_after_data('E', 'E1')
+# ll.insert_after_data('C', 'C1')
+# ll.insert_after_data('B', 'B1')
+# ll.printAll()
+
 # print("length: {}".format(ll.length()))
 # ll.delete_tail()
 # ll.printAll()
